c7c1a60270a146125d7c2ef308e66dff94c431dc
kowaraj <kowaraj4stuff@gmail.com>
2016-04-05 18:11:28 +0200

    started pixelizing

--- snake_game.py
+++ snake_game.py
@@ -4,52 +4,28 @@ import time, threading
 import random
 from snake import Snake
 from ptitpois import Ptitpois
+from world import World
 
-step = 45
-
-fen = Tk()
-fen.geometry("600x600")
-canv=Canvas(fen, bg='black', height=500,width=500)
-canv.place(x=0, y=0)
-
-vasya = Snake(canv, step, step)
-vasya.name = 'shinigami'
-
-Ptitpois(canv, random.randint(0,500) / step * step , random.randint(0,500) / step * step)
-Ptitpois(canv, random.randint(0,500) / step * step , random.randint(0,500) / step * step )
+w = World()
 
 
+w.start()
 
-print 'Vasya\'s name: ', vasya.name
+# step = 45
 
-def change_direction(event):
-    print 'tu a appuye le bouton:' + event.char
-    print 'snake dir changed'
-    key = event.char
-    if key == "d":
-        vasya.dir="right"
-    if key == "a":
-        vasya.dir="left"
-    if key == "w":
-       vasya.dir="up"
-    if key == "s":
-        vasya.dir="down"
+# fen = Tk()
+# fen.geometry("600x600")
+# canv=Canvas(fen, bg='black', height=500,width=500)
+# canv.place(x=0, y=0)
 
+# vasya = Snake(canv, step, step)
+# vasya.name = 'shinigami'
 
-def move_snake(event):
-    print 'tu a appuye le bouton:' + event.char
-    print 'snake moved to... '
-    key = event.char
-    if key == "d":
-        vasya.move_right()
-    if key == "a":
-        vasya.move_left()
-    if key == "w":
-       vasya.move_up()
-    if key == "s":
-        vasya.move_down()
+# Ptitpois(canv, random.randint(0,500) / step * step , random.randint(0,500) / step * step)
+# Ptitpois(canv, random.randint(0,500) / step * step , random.randint(0,500) / step * step )
 
 
-fen.bind('<KeyPress>', change_direction)
-fen.mainloop()
+
+
+
 
--- world.py
+++ world.py
@@ -2,26 +2,70 @@ from Tkinter import *
 import random
 
 class Ground(Canvas):
+
+    step = 50
+    h = 500
+    w = 500
+    xs = range(w/step)
+    ys = range(h/step)
     
-    def __init__(self, space, h, w, b):
-        self.h = h - 2*b
-        self.w = w - 2*b
+    def __init__(self, space):
         Canvas.__init__(self, space, bg='black', height=self.h, width=self.w)
-        self.place(x=b, y=b)
-    
+        self.place(x=0, y=0)
+        self.coords = [ [x,y] for x in self.xs for y in self.ys]
+
+    def draw(self):
+        for y in self.ys:
+            self.draw_parallel(y)
+        for x in self.xs:
+            self.draw_meridian(x)
+            
+    def draw_parallel(self, i):
+        y = 0 + i * self.step
+        print('y = ', y)
+        self.create_line(0, y, self.w, y, fill="dark grey")
+        
+    def draw_meridian(self, i):
+        x = 0 + i * self.step
+        print('x = ', x)
+        self.create_line(x, 0, x, self.h, fill="dark grey")
+
+
 class World:
 
-    border = 10
-    def __init__(self, size=[600,600]):
-        self.h = size[0]
-        self.w = size[1]
+    s = 50
+    h = 500
+    w = 500
+    
+    def __init__(self):
         # window
         self.space = Tk()
         self.space.geometry(str(self.h)+"x"+str(self.w))
         # canvas
-        self.ground = Ground(self.space, self.h, self.w, self.border)
+        self.ground = Ground(self.space)
+        self.ground.draw()
+        self.cells = [ x+y*10  for x in self.xs for y in self.ys]
 
+        
+        
     def rand(self):
         rand_y = random.randint(self.border, self.ground.h)
         rand_x = random.randint(self.border, self.ground.w)
         return [rand_x, rand_y]
+
+    def change_direction(event):
+        print 'tu a appuye le bouton:' + event.char
+        print 'snake dir changed'
+        key = event.char
+        if key == "d":
+            vasya.dir="right"
+        if key == "a":
+            vasya.dir="left"
+        if key == "w":
+            vasya.dir="up"
+        if key == "s":
+            vasya.dir="down"
+        
+    def start(self):
+        self.space.bind('<KeyPress>', self.change_direction)
+        self.space.mainloop()
